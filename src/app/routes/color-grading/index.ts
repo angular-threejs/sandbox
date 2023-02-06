cfae85d582eaf59a66a7036718525d198e2ead45
Chau Tran <nartc7789@gmail.com>
2023-02-06 12:28:38 -0600

    add color grading

---
+++ src/app/routes/color-grading/index.ts
@@ -0,0 +1,55 @@
+import { RouteMeta } from '@analogjs/router';
+import { NgIf } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { injectNgtLoader, NgtArgs, NgtCanvas, NgtPush } from 'angular-three';
+import { NgtpEffectComposer } from 'angular-three-postprocessing';
+import { NgtpLUT } from 'angular-three-postprocessing/effects';
+import { NgtsOrbitControls } from 'angular-three-soba/controls';
+import { injectNgtsTextureLoader } from 'angular-three-soba/loaders';
+import { NgtsEnvironment } from 'angular-three-soba/staging';
+import { LookupTexture, LUTCubeLoader } from 'postprocessing';
+import { Observable } from 'rxjs';
+
+export const routeMeta: RouteMeta = {
+    title: 'Color Grading',
+};
+
+@Component({
+    selector: 'GradingSphere',
+    standalone: true,
+    templateUrl: 'sphere.html',
+    imports: [NgtArgs, NgtPush, NgIf],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class GradingSphere {
+    readonly texture$ = injectNgtsTextureLoader('terrazo.png');
+}
+
+@Component({
+    selector: 'Grading',
+    standalone: true,
+    templateUrl: 'grading.html',
+    imports: [NgtpLUT, NgtpEffectComposer, NgtPush, NgIf],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class Grading {
+    readonly texture$ = injectNgtLoader(() => LUTCubeLoader as any, 'cubicle-99.CUBE') as Observable<LookupTexture>;
+}
+
+@Component({
+    standalone: true,
+    templateUrl: 'scene.html',
+    imports: [GradingSphere, Grading, NgtsEnvironment, NgtsOrbitControls],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class Scene {}
+
+@Component({
+    standalone: true,
+    templateUrl: 'color-grading.html',
+    imports: [NgtCanvas],
+    host: { class: 'block h-full w-full bg-gradient-to-br from-indigo-300 to-sky-700' },
+})
+export default class ColorGrading {
+    readonly scene = Scene;
+}
